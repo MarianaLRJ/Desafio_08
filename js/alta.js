d04a920ce5c3161bc253f48acfb482b92c17b60e
MarianaLRJ <marianadeoliveira384@gmail.com>
2024-10-01 16:06:10 -0300

    listo index

--- js/alta.js
+++ js/alta.js
@@ -1,9 +1,8 @@
 let email = 'alta@gmail.com';
 let password = '1234';
-alert('BIENVENIDO A LA CARGA DE PRODUCTOS');
 
 
-alert ('Por favor, confirma tu mail!');
+alert ('Hola!!! Por favor confirma tu mail!');
 
 let emailUser = prompt('Introduce tu Email: ');
 let passwordUser = prompt('Introduce tu Password: ');
@@ -11,10 +10,22 @@ let passwordUser = prompt('Introduce tu Password: ');
 console.log(window);
 
 if(email == emailUser && password == passwordUser){
-    console. log ('Bienvenido a tu cuenta')
-alert ('Bienvenido a la página Alta');
+    console. log ('Bienvenid@ a tu cuenta')
+alert ('Bienvenid@ a tu cuenta');
+ window.location= "../Pages.html/Categorías.html"
 } else{
     alert ("El mail/password no coinciden")
     window.location= "../index.html"
 }
 
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
