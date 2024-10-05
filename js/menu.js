c88e64c4decc561161a1617e3f3ca80f82afef3c
Matias <mativillalba03@gmail.com>
2024-10-05 19:38:52 -0300

    termine js

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,19 @@
 const opciones = document.querySelectorAll(".card");
 
 let carrito = [];
+let preciototal = 0;
+let tituloProducto = "";
+let precioProducto = 0;
+const modal = document.querySelector(".container-modal");
+const agregar = modal.querySelector(".agregar");
+
 
 opciones.forEach((opcion) => {
   opcion.addEventListener(`click`, (evento) => {
     const titulo = opcion.querySelector(".negro").textContent;
-    const precio = opcion.querySelector(".precio").textContent;
-
-    const modal = document.querySelector(".container-modal");
+    tituloProducto = titulo;
+    const precio = opcion.querySelector(".precioProducto").textContent;
+    precioProducto = precio;
 
     modal.style.display = "flex";
 
@@ -15,35 +21,45 @@ opciones.forEach((opcion) => {
 
     titulomodal.textContent = titulo;
 
+    const precioModal = modal.querySelector(".precioModal");
+
+    precioModal.textContent = precio;
+
     const cerrarmodal = modal.querySelector(".cerrar");
 
     cerrarmodal.addEventListener("click", () => {
       modal.style.display = "none";
     });
-
-    const agregar = modal.querySelector(".agregar");
-
-    agregar.addEventListener("click", () => {
-      const cantidad = modal.querySelector("#Cantidad").value;
-
-      const comentario = modal.querySelector("#Comentario").value;
-
-      const obj = {
-        titulo: titulo,
-        precio: precio,
-        cantidad: cantidad,
-        comentario: comentario,
-      };
-
-      carrito.push(obj);
-
-      cantidad.value = "";
-      comentario.value = "";
-
-      cerrarmodal.click();
-    });
   });
 });
 
 
+agregar.addEventListener("click", () => {
+  const cantidad = modal.querySelector("#Cantidad").value;
 
+  const comentario = modal.querySelector("#Comentario").value;
+
+  const obj = {
+    titulo: tituloProducto,
+    precio: precioProducto,
+    cantidad: cantidad,
+    comentario: comentario,
+  };
+
+  carrito.push(obj);
+
+  cantidad.value = "";
+  comentario.value = "";
+
+  const numeroCarrito = document.querySelector(".numeroCarrito");
+
+
+  preciototal = preciototal + cantidad * precioProducto;
+
+
+  numeroCarrito.textContent = preciototal;
+
+  const cerrarmodal = modal.querySelector(".cerrar");
+
+  cerrarmodal.click();
+});
